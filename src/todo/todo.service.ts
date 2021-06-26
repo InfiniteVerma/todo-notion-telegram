931abcfa96c04b03b89c7fd26383ee8a132bf77c
infiniteverma <vermaanant25@gmail.com>
2021-06-26 09:57:18 +0530

    remove unused env

--- src/todo/todo.service.ts
+++ src/todo/todo.service.ts
@@ -5,7 +5,6 @@ import { todoListToString } from "./todo.interface";
 dotenv.config()
 
 const notion = new Client({ auth: process.env.NOTION_KEY })
-const databaseId = process.env.NOTION_DATABASE_ID
 const blockID = process.env.NOTION_BLOCK_ID;
 
 async function listTodoItems() {
