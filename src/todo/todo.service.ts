ae40e0f03b2d3aa8d3b0dba4381025ce1aab7f38
infiniteverma <vermaanant25@gmail.com>
2022-03-26 10:43:52 +0530

    reformat, fix type error in err obj

--- src/todo/todo.service.ts
+++ src/todo/todo.service.ts
@@ -1,69 +1,78 @@
 import { Client } from "@notionhq/client";
 import { ToDoBlock } from "@notionhq/client/build/src/api-types";
-import * as dotenv from 'dotenv';
+import * as dotenv from "dotenv";
 import { todoListToString } from "./todo.interface";
-dotenv.config()
+dotenv.config();
 
-const notion = new Client({ auth: process.env.NOTION_KEY })
+const notion = new Client({ auth: process.env.NOTION_KEY });
 const blockID = process.env.NOTION_BLOCK_ID;
 
-async function listTodoItems() {
-    try {
-        const response = await notion.blocks.children.list({
-            block_id: blockID!,
-        });
+const listTodoItems = async () => {
+  try {
+    const response = await notion.blocks.children.list({
+      block_id: blockID!,
+    });
 
-        const texts = response.results.map((ele) => ele as ToDoBlock).map((item) => ({
-            'text': item.to_do.text[0].plain_text, 'isChecked': item.to_do.checked
-        }))
+    const texts = response.results
+      .map((ele) => ele as ToDoBlock)
+      .map((item) => ({
+        text: item.to_do.text[0].plain_text,
+        isChecked: item.to_do.checked,
+      }));
 
-        return todoListToString(texts);
-    } catch (error) {
-        console.error(error.body)
-    }
-}
+    return todoListToString(texts);
+  } catch (error: any) {
+    console.error(error.body);
+  }
+};
 
-async function addTodoItem(item: string) {
-    try {
-        const response = await notion.blocks.children.append({
-            block_id: blockID!,
-            children: [
-                {
-                    id: "asdf",
-                    created_time: Date.now().toString(),
-                    last_edited_time: Date.now().toString(),
-                    object: 'block',
-                    type: 'to_do',
-                    has_children: false,
-                    to_do: {
-                        text: [{
-                            type: "text",
-                            text: {
-                                content: item,
-                            },
-                            plain_text: item,
-                            annotations: {
-                                bold: false,
-                                italic: false,
-                                strikethrough: false,
-                                underline: false,
-                                code: false,
-                                color: "default"
-                            }
-                        }],
-                        checked: false,
+const addTodoItem = async (item: string) => {
+  try {
+    const response = await notion.blocks.children.append({
+      block_id: blockID!,
+      children: [
+        {
+          id: "asdf",
+          created_time: Date.now().toString(),
+          last_edited_time: Date.now().toString(),
+          object: "block",
+          type: "to_do",
+          has_children: false,
+          to_do: {
+            text: [
+              {
+                type: "text",
+                text: {
+                  content: item,
+                },
+                plain_text: item,
+                annotations: {
+                  bold: false,
+                  italic: false,
+                  strikethrough: false,
+                  underline: false,
+                  code: false,
+                  color: "default",
+                },
+              },
+            ],
+            checked: false,
+          },
+        },
+      ],
+    });
 
-                    }
-                }
-            ]
-        })
+    console.log(response);
 
-        console.log(response);
+    return response;
+  } catch (error: any) {
+    console.error(error.body);
+  }
+};
 
-        return response;
-    } catch (error) {
-        console.error(error.body)
-    }
-}
+const clearTodoList = () => {
+  try {
+  } catch (e) {}
+};
 
-export { listTodoItems, addTodoItem };
+export { listTodoItems, addTodoItem, clearTodoList };
